882bc2db25ba992c8216fd2b3157dedc44434d20
crowends <hjosealejandro21@gmail.com>
2025-05-06 21:49:50 -0600

    refactor(commands): :bug: responsibilities into different classes in user_password.py

--- utils/variable_globals.py
+++ utils/variable_globals.py
@@ -76,8 +76,7 @@ from django.contrib.auth.views import PasswordChangeView, PasswordResetView, Pas
 from django.urls import reverse_lazy
 from django.contrib import messages
 
-class PasswordViews:
-    class Change(PasswordChangeView):
+class UserPasswordChange(PasswordChangeView):
         template_name = "registration/password/change-password.html"
         success_url = reverse_lazy('profile')
 
@@ -85,7 +84,7 @@ class PasswordViews:
             messages.success(self.request, "La contraseña se ha cambiado exitosamente.")
             return super().form_valid(form)
 
-    class Reset(PasswordResetView):
+class UserPasswordReset(PasswordResetView):
         template_name = "registration/password/reset_password_email.html"
         success_url = reverse_lazy('profile')
         email_template_name = "registration/password/password_reset_email.html"
@@ -94,11 +93,11 @@ class PasswordViews:
             messages.success(self.request, "Se ha enviado un correo electrónico a tu bandeja de entrada.")
             return super().form_valid(form)
 
-    class Confirm(PasswordResetConfirmView):
+class UserPasswordConfirm(PasswordResetConfirmView):
         template_name = "registration/password/reset_confirm.html"
         success_url = reverse_lazy('reset-password-done')
 
-    class Complete(PasswordResetCompleteView):
+class UserPasswordComplete(PasswordResetCompleteView):
         template_name = "registration/password/reset_password_complete.html"
 """
 
