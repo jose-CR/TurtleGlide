bee3200489057747fae11634aaa5c18605d62fe5
crowends <hjosealejandro21@gmail.com>
2025-05-06 21:34:06 -0600

    fix(commands): :bug: ✅ Fix missing class in user_password.py

--- utils/variable_globals.py
+++ utils/variable_globals.py
@@ -72,109 +72,34 @@ class CustomUserChangeForm(UserChangeForm):
 """
 
 user_password = """
-from home.forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
-from django.http import Http404
 from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 
-# Create perfil
-class ProfileCreateView(CreateView):
-    model = User
-    form_class = CustomUserCreationForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('app')
+class PasswordViews:
+    class Change(PasswordChangeView):
+        template_name = "registration/password/change-password.html"
+        success_url = reverse_lazy('profile')
 
-    def form_valid(self, form):
-        self.object = form.save()
-        success_url = reverse_lazy('app')
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(self.request, username=username, password=password)
-        if user:
-            login(self.request, user)
-            
-        return redirect(success_url)
+        def form_valid(self, form):
+            messages.success(self.request, "La contraseña se ha cambiado exitosamente.")
+            return super().form_valid(form)
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['title'] = 'Register'
-        context['button'] = 'Sing In'
-        return context
+    class Reset(PasswordResetView):
+        template_name = "registration/password/reset_password_email.html"
+        success_url = reverse_lazy('profile')
+        email_template_name = "registration/password/password_reset_email.html"
 
-# Read perfil
-class Profile():
-    def exit(request):
-        logout(request)
-        return redirect('app')
+        def form_valid(self, form):
+            messages.success(self.request, "Se ha enviado un correo electrónico a tu bandeja de entrada.")
+            return super().form_valid(form)
 
-    @login_required
-    def index(request):
+    class Confirm(PasswordResetConfirmView):
+        template_name = "registration/password/reset_confirm.html"
+        success_url = reverse_lazy('reset-password-done')
 
-        context = {
-            'label_username': 'Usuario',
-            'label_first_name': 'Primer Nombre',
-            'label_last_name': 'Último Apellido',
-            'label_email': 'Correo Electrónico',
-        }
-
-        return render(request, 'profile.html', context)
-    
-# Update perfil
-class ProfileUpdateView(UpdateView):
-    model = User
-    form_class = CustomUserChangeForm
-    template_name = 'profile/profile-edit.html'
-    success_url = reverse_lazy('profile')
-
-    def get_object(self, queryset=None):
-        response = super().get_object(queryset)
-        user_id = self.kwargs.get('pk')
-        if self.request.user.id == user_id:
-            return self.request.user
-        raise Http404("No puedes editar otro usuario.")
-
-    def get_queryset(self):
-        return User.objects.filter(pk=self.request.user.pk)
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, 'Tu perfil se ha actualizado correctamente.')
-        return response
-
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['title'] = 'Edit User'
-        context['button'] = 'editing'
-        context['user_id'] = self.request.user.id
-        return context
-    
-# Delete perfil 
-class ProfileDeleteView(DeleteView):
-    model = User
-    template_name = 'profile/profile-delete.html'
-    success_url = reverse_lazy('app')
-
-    def get_queryset(self):
-        return User.objects.filter(pk=self.request.user.pk)
-
-    def delete(self, request, *args, **kwargs):
-        response = super().delete(request, *args, **kwargs)
-        logout(request)
-
-        return response
-
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['title'] = 'Delete User'
-        context['button'] = 'deleting'
-        return context
-
-
+    class Complete(PasswordResetCompleteView):
+        template_name = "registration/password/reset_password_complete.html"
 """
 
 user_profile = """
@@ -279,8 +204,6 @@ class ProfileDeleteView(DeleteView):
         context['title'] = 'Delete User'
         context['button'] = 'deleting'
         return context
-
-
 """
 
 test_helpers = """
@@ -325,8 +248,6 @@ def call_status(response=None, *, client=None, url_name=None, expected_status=20
         print(f"❌ [{url_name.upper() if url_name else ''}] FALLÓ - Esperado: {expected_status}, Recibido: {response.status_code} → {message}")
 
     return response
-
-
 """
 
 test_profile = """
