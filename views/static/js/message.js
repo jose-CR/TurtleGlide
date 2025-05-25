3c4ea859f6283f6af543dfb5b17c4cf6e204b95b
crowends <hjosealejandro21@gmail.com>
2025-05-24 20:40:37 -0600

    style(command): :lipstick: remove view directory and add variables_global.py to utils

--- views/static/js/message.js
+++
@@ -1,7 +0,0 @@
-function closeMessage(button) {
-    const message = button.parentElement;
-    message.classList.add('fade-out');
-    setTimeout(() => {
-        message.style.display = 'none';
-    }, 300);
-}
